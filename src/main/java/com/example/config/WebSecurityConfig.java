97fe801a1b938831fbbeca947a7042a558a5008a
金正朋也 <mhtnim1023@gmail.com>
2019-06-27 01:19:48 +0900

    csrf対策

---
+++ src/main/java/com/example/config/WebSecurityConfig.java
@@ -0,0 +1,20 @@
+//package com.example.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+//
+//
+//// セキュリティ関連のバリデーションを行うクラス
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        // csrfの管理を行ったり、url別のアクセス許可を行ったりできる
+////        hthtttptp.authorizeRequests().antMatchers()
+////        http.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+//    }
+//}
--- src/main/java/com/example/controller/HomeController.java
+++ src/main/java/com/example/controller/HomeController.java
@@ -5,9 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 
 @Controller
-public class HelloController {
+public class HomeController {
     @GetMapping("/")
     public String index() {
+        // sessionの確認
+        // あれば、index.htmlに返す
+        // なければ、sign_up画面に飛ばす
         System.out.println("welcome to index action");
         return "index";
     }
